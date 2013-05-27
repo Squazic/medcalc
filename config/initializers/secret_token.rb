b98e9a6369d4e733aeb96596bafd9c0d0752501d
Steven Chen <szchen77@gmail.com>
2013-05-27 17:37:13 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Medcalc::Application.config.secret_token = 'fdf1013ff8f7d5ae6d27386b66d91b6e8ba1ee1ff212220060bf0283fda5cb117d5441c4de3546936db44caa4b42c83c388544154fd194c45c97e28eb172ca03'

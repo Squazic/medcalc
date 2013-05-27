cac098fdd982af2d8be4c927f3ac2b761b7521d3
Steven Chen <szchen77@gmail.com>
2013-05-27 18:41:43 -0400

    Add basic validation

--- app/models/framingham_profile.rb
+++ app/models/framingham_profile.rb
@@ -1,3 +1,7 @@
 class FraminghamProfile < ActiveRecord::Base
   attr_accessible :age, :bmi, :diabetes, :male, :sbp, :smoker, :treatment
+
+  validates :age, :presence => true
+  validates :bmi, :presence => true
+  validates :sbp, :presence => true
 end

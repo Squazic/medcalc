b6ed7725d6053f6d08a0bcf69067ae026785a483
Steven Chen <szchen77@gmail.com>
2013-05-27 23:53:15 -0400

    Basic unit testing

--- test/unit/framingham_profile_test.rb
+++ test/unit/framingham_profile_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class FraminghamProfileTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save profile without age" do
+    fp = FraminghamProfile.new
+    fp.sbp = 100
+    fp.bmi = 30
+    assert !fp.save
+  end
+  test "should not save profile without sbp" do
+    fp = FraminghamProfile.new
+    fp.age = 30
+    fp.bmi = 23
+    assert !fp.save
+  end
+  test "should not save profile without bmi" do
+    fp = FraminghamProfile.new
+    fp.sbp = 100
+    fp.age = 30
+    assert !fp.save
+  end
 end

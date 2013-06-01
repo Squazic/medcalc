f02c6406076f3a1e763ef8873fde14925364bb98
Steven Chen <szchen77@gmail.com>
2013-06-01 00:09:19 -0400

    Add unit checking to input for height/weight

--- app/models/framingham_profile.rb
+++ app/models/framingham_profile.rb
@@ -1,5 +1,7 @@
 class FraminghamProfile < ActiveRecord::Base
   attr_accessible :age, :diabetes, :male, :sbp, :smoker, :treatment, :height, :weight
+  attr_accessor :height_units, :weight_units
+  attr_accessible :height_units, :weight_units
 
   validates_presence_of :age, :message => "Don't be shy now :)"
   validates_inclusion_of :diabetes, :in => [true, false], :message => "We promise we won't tell."
@@ -9,4 +11,21 @@ class FraminghamProfile < ActiveRecord::Base
   validates_inclusion_of :treatment, :in => [true, false], :message => "Missed one."
   validates_presence_of :height, :message => "Come on, we know you're as tall as Michael Jordan"
   validates_presence_of :weight, :message => "Don't be embarrassed. No one's going to see this but you :)"
+  validates_presence_of :height_units, :message => "Don't forget the units!"
+  validates_presence_of :weight_units, :message => "Are you a metric guy?"
+
+  before_create :convert_height, :convert_weight
+
+  private
+    def convert_height
+      if self.height_units == "in"
+        self.height *= 2.54
+      end
+    end
+
+    def convert_weight
+      if self.weight_units == "lb"
+        self.weight *= 0.4536
+      end
+    end
 end

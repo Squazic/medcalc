e009ce8710e302286f40ac391fe5a8b0f891d4b9
Steven Chen <szchen77@gmail.com>
2013-05-31 22:58:32 -0400

    Fix validation with boolean fields

--- app/models/framingham_profile.rb
+++ app/models/framingham_profile.rb
@@ -2,11 +2,11 @@ class FraminghamProfile < ActiveRecord::Base
   attr_accessible :age, :diabetes, :male, :sbp, :smoker, :treatment, :height, :weight
 
   validates_presence_of :age, :message => "Don't be shy now :)"
-  validates_presence_of :diabetes, :message => "We won't tell"
-  validates_presence_of :male, :message => "Pick one :)"
+  validates_inclusion_of :diabetes, :in => [true, false], :message => "We promise we won't tell."
+  validates_inclusion_of :male, :in => [true, false], :message => "Pick one :)"
   validates_presence_of :sbp, :message => "Looks like you forgot something :)"
-  validates_presence_of :smoker, :message => "Don't forget this one."
-  validates_presence_of :treatment, :message => "Missed one."
+  validates_inclusion_of :smoker, :in => [true, false], :message => "Don't forget this one."
+  validates_inclusion_of :treatment, :in => [true, false], :message => "Missed one."
   validates_presence_of :height, :message => "Come on, we know you're as tall as Michael Jordan"
   validates_presence_of :weight, :message => "Don't be embarrassed. No one's going to see this but you :)"
 end
